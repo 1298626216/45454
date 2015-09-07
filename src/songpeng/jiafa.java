f1a0dc560a70c078df7c375d8f3be35f3c187ac6
784740346 <784740346@784740346-PC>
2015-09-07 19:27:09 +0800

    项目提交

---
+++ src/songpeng/jiafa.java
@@ -0,0 +1,8 @@
+package songpeng;
+
+public class jiafa {
+ public int sum (int a, int b){
+	 int resultSum = a + b;
+	 return resultSum;
+ }
+}
---
+++ src/songpeng/jiafaTest.java
@@ -0,0 +1,14 @@
+package songpeng;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class jiafaTest {
+
+	@Test
+	public void testSum() {
+		fail("Not yet implemented");
+	}
+
+}
